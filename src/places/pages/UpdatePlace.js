58ec2ed7ed2ff07d5b355014a8c05d06942c85df
Anoop S Mohan <anoop4439@gmail.com>
2021-02-04 12:30:26 +0530

    Frontend and backend connected

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,44 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams, useHistory } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 import './PlaceForm.css';
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Kollam',
-        imageUrl: 'https://img.traveltriangle.com/blog/wp-content/uploads/2018/04/Jatayu%E2%80%99s-Earth-Centre-kollam-kb6592.jpg',
-        description: 'Beautiful place in Kerala',
-        address: 'Kerala, India',
-        location: {
-            lat: 8.9040558,
-            lng: 76.5250328
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Kollam',
-        imageUrl: 'https://cdn.s3waas.gov.in/s39fc3d7152ba9336a670e36d0ed79bc43/uploads/2018/04/2018040320-1024x768.jpg',
-        description: 'Beautiful place in Kerala',
-        address: 'Kerala, India',
-        location: {
-            lat: 8.9040558,
-            lng: 76.5250328
-        },
-        creator: 'u2'
-    }
-]
-
 const UpdatePlace = () => {
+    const auth = useContext(AuthContext);
     const placeId = useParams().placeId;
-
-    const [isLoading, setIsLoading] = useState(true);
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const [loadedPlace, setLoadedPlace] = useState();
+    const history = useHistory();
 
     const [formState, inputHandler, setFormData] = useForm({
         title: {
@@ -51,47 +30,67 @@ const UpdatePlace = () => {
         }
     }, false);
 
-    const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
-
     useEffect(() => {
-        if (identifiedPlace) {
-            setFormData({
-                title: {
-                    value: identifiedPlace.title,
-                    isValid: true
-                },
-                description: {
-                    value: identifiedPlace.description,
-                    isValid: true
-                }
-            }, true);
-        }
-        setIsLoading(false);
-    }, [setFormData, identifiedPlace])
+        const fetchPlace = async () => {
+            try {
+                const responseData = await sendRequest(`http://localhost:5000/api/places/${placeId}`);
+                setLoadedPlace(responseData.place);
+                setFormData({
+                    title: {
+                        value: responseData.place.title,
+                        isValid: true
+                    },
+                    description: {
+                        value: responseData.place.description,
+                        isValid: true
+                    }
+                }, true);
+            } catch (err) {}
+        };
+        fetchPlace();
+    }, [sendRequest, placeId, setFormData]);
 
-    const placeUpdateSubmitHandler = event => {
+
+    const placeUpdateSubmitHandler = async event => {
         event.preventDefault();
-        console.log(formState.inputs);
+        try {
+            await sendRequest(
+                `http://localhost:5000/api/places/${placeId}`,
+                'PATCH',
+                JSON.stringify({
+                    title: formState.inputs.title.value,
+                    description: formState.inputs.description.value
+                }),
+                { 'Content-Type': 'application/json'}
+            )
+            history.push('/' + auth.userId + '/places');
+        } catch (err) {}
     }
 
     if (isLoading) {
         return <div className="center">
-            <h2>Loading...</h2>
+            <LoadingSpinner />
         </div>
     }
 
-    if (!identifiedPlace) {
+    if (!isLoading && !loadedPlace) {
         return <div className="center">
             <Card>
                 <h2>Could not find place</h2>
             </Card>
         </div>
     }
-    return <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
-        <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title." onInput={inputHandler} initialValue={formState.inputs.title.value} initialValid={formState.inputs.title.isValid} />
-        <Input id="description" element="textarea" label="Description" validators={[VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid description (min. 5 characters)." onInput={inputHandler} initialValue={formState.inputs.description.value} initialValid={formState.inputs.description.isValid} />
-        <Button type="submit" disabled={!formState.isValid}>Update Place</Button>
-    </form>
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            {!isLoading && loadedPlace && <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+                <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title." onInput={inputHandler} initialValue={loadedPlace.title} initialValid={true} />
+                <Input id="description" element="textarea" label="Description" validators={[VALIDATOR_MINLENGTH(5)]} errorText="Please enter a valid description (min. 5 characters)." onInput={inputHandler} initialValue={loadedPlace.description} initialValid={true} />
+                <Button type="submit" disabled={!formState.isValid}>Update Place</Button>
+            </form>}
+        </React.Fragment>
+    
+    );
 };
 
 export default UpdatePlace;
