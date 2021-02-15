f6b59f4d6f652c41ddf4962725a0845d7c05bb1a
Anoop S Mohan <anoop4439@gmail.com>
2021-02-15 20:28:00 +0530

    cleaned the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-// import Users from './user/pages/Users';
-// import NewPlace from './places/pages/NewPlace';
-// import UserPlaces from './places/pages/UserPlaces';
-// import UpdatePlace from './places/pages/UpdatePlace';
-// import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import LoadingSpinner from './shared/components/UIElements/LoadingSpinner';
 import { AuthContext } from './shared/context/auth-context';
